ee31de9fcd3fd40925a49b82e82fa658f958e5c7
Gabriel <Gabriel@DESKTOP-ILJ01CL>
2023-05-15 19:39:40 -0300

    Finalizado?

--- FerreiroPadronizado/src/ferreiropadronizado/FerreiroPadronizado.java
+++ FerreiroPadronizado/src/ferreiropadronizado/FerreiroPadronizado.java
@@ -1,21 +1,33 @@
 package ferreiropadronizado;
 
+import ferreiropadronizado.forjas.ForjaAbstrata;
+import ferreiropadronizado.forjas.ForjaAco;
+import ferreiropadronizado.forjas.ForjaBronze;
+import ferreiropadronizado.forjas.ForjaFerro;
 
 
-/**
- *
- * @author Gabriel
- */
 public class FerreiroPadronizado {
 
-    
-    
-    
-    public static void main(String[] args) {
+
+public static void main(String[] args) {
+        Application app;
+        ForjaAbstrata factory;
+        String material = "Bronze";
+
+        if (material.contains("Ferro")) {
+            factory = new ForjaFerro();
+
+        } else if (material.contains("Aço")) {
+            factory = new ForjaAco();
+            
+        } else {
+            factory = new ForjaBronze();
+        }
         
-       
+        app = new Application(factory);
         
         
+        
+        app.Finalizado();
     }
-    
 }
